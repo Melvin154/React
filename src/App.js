82b24d788b665e09d2558ee698df071dd85ce4a8
Melvin154 <melthomas2202gmail.com>
2023-01-25 00:00:43 +0530

    First commit in this project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To Xplore With JS</h1>
+      <h2>Learn React in Tamil</h2>
+    </div>
+  );
+}
+
+export default App;
