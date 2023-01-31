d8081c3bf28cb76369720322cd10b61240cfb7dd
Melvin154 <melthomas2202gmail.com>
2023-01-31 23:05:03 +0530

    New Code Added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css';
-import styles from './demo.module.css'
+//import "./index.css";
+//import styles from "./demo.module.css";
 
 //import App from './App.js';
 //Components
@@ -177,7 +177,7 @@ ReactDOM.render(<Eventbind />, document.getElementById("root"));
 */
 
 //React CSS
-
+/*
 class Reactstyle extends React.Component {
   render() {
     const mystyle={
@@ -195,3 +195,43 @@ class Reactstyle extends React.Component {
   }
 }
 ReactDOM.render(<Reactstyle />, document.getElementById("root"));
+*/
+
+//React Forms
+class Reactform extends React.Component {
+  constructor() {
+    super();
+    this.state = { username: "", age: null, errmsg: "" };
+  }
+  uservalue = (event) => {
+    let n = event.target.name;
+    let v = event.target.value;
+    let err = " ";
+      if (n === "age") {
+        if (v !== "" && !Number(v)) {
+          err = <strong>Invalid Value , Your age must be A Number!!</strong>;
+        }
+      }
+  
+    this.setState({ errmsg: err});
+    this.setState({ [n]: v });
+  };
+  formsubmit=(event)=>{
+    event.preventDefault();
+    alert("Your Name is " + this.state.username + "And Your Age is " + this.state.age)
+  }
+  render() {
+    return (
+      <form onSubmit={this.formsubmit}>
+        <h1>Hello {this.state.username}</h1>
+        <h2>Your Age {this.state.age}</h2>
+        Enter Your Name :
+        <input type="text" name="username" onChange={this.uservalue}></input>
+        Enter Your Age :
+        <input type="text" name="age" onChange={this.uservalue}/>{this.state.errmsg}
+        <input type="submit"/>
+      </form>
+    );
+  }
+}
+ReactDOM.render(<Reactform />, document.getElementById("root"));
